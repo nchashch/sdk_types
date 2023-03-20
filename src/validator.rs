cb4aab65894307b537d8b17adc56de97354c8692
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-03-20 21:45:57 +0400

    Simplify validation API

--- src/validator.rs
+++ src/validator.rs
@@ -2,16 +2,8 @@ use crate::types::*;
 use serde::Serialize;
 use std::collections::HashSet;
 
-pub trait CustomValidator<A, C> {
-    fn custom_validate_transaction(
-        &self,
-        spent_utxos: &[Output<C>],
-        transaction: &Transaction<A, C>,
-    ) -> Result<(), String>;
-}
-
 // Returns the fee paid by transaction if it is valid.
-pub fn regular_validate_transaction<A: GetAddress, C: GetValue>(
+pub fn validate_transaction<A: GetAddress, C: GetValue>(
     spent_utxos: &[Output<C>],
     transaction: &Transaction<A, C>,
 ) -> Result<u64, String> {
@@ -34,48 +26,46 @@ pub fn regular_validate_transaction<A: GetAddress, C: GetValue>(
     Ok(value_in - value_out)
 }
 
-pub trait Validator<A: GetAddress + Serialize, C: GetValue + Clone + Serialize>:
-    CustomValidator<A, C>
-{
+// Returns total fee collected by body if it is valid.
+fn validate_body<A: GetAddress + Clone + Serialize, C: GetValue + Clone + Serialize>(
+    spent_utxos: &[Vec<Output<C>>],
+    body: &Body<A, C>,
+) -> Result<u64, String> {
+    let mut fees: u64 = 0;
+
+    // No UTXO is double spent within the same body.
+    let mut seen_inputs: HashSet<OutPoint> = body
+        .transactions
+        .iter()
+        .flat_map(|transaction| transaction.inputs.iter().copied())
+        .collect();
+    for input in body
+        .transactions
+        .iter()
+        .flat_map(|transaction| transaction.inputs.iter())
+    {
+        if seen_inputs.contains(input) {
+            return Err("utxo is double spent in transaction".into());
+        }
+        seen_inputs.insert(*input);
+    }
+    for (transaction, spent_utxos) in body.transactions.iter().zip(spent_utxos.iter()) {
+        fees += validate_transaction(spent_utxos, transaction)?;
+    }
+    if body.get_coinbase_value() > fees {
+        return Err("coinbase value > fees".into());
+    }
+    Ok(fees)
+}
+
+pub trait State {
+    type Authorization;
+    type Custom;
+    type Error;
     fn validate_transaction(
         &self,
-        spent_utxos: &[Output<C>],
-        transaction: &Transaction<A, C>,
-    ) -> Result<u64, String> {
-        self.custom_validate_transaction(spent_utxos, transaction)?;
-        regular_validate_transaction(spent_utxos, transaction)
-    }
-    // Returns total fee collected by body if it is valid.
-    fn validate_body(
-        &self,
-        spent_utxos: &[Vec<Output<C>>],
-        body: &Body<A, C>,
-    ) -> Result<u64, String> {
-        let mut fees: u64 = 0;
-
-        // No UTXO is double spent within the same body.
-        let mut seen_inputs: HashSet<OutPoint> = body
-            .transactions
-            .iter()
-            .flat_map(|transaction| transaction.inputs.iter().copied())
-            .collect();
-        for input in body
-            .transactions
-            .iter()
-            .flat_map(|transaction| transaction.inputs.iter())
-        {
-            if seen_inputs.contains(input) {
-                return Err("utxo is double spent in transaction".into());
-            }
-            seen_inputs.insert(*input);
-        }
-
-        for (transaction, spent_utxos) in body.transactions.iter().zip(spent_utxos.iter()) {
-            fees += self.validate_transaction(spent_utxos, transaction)?;
-        }
-        if body.get_coinbase_value() > fees {
-            return Err("coinbase value > fees".into());
-        }
-        Ok(fees)
-    }
+        spent_outputs: &[Output<Self::Custom>],
+        transaction: &Transaction<Self::Authorization, Self::Custom>,
+    ) -> Result<(), Self::Error>;
+    fn connect_outputs(&mut self, outputs: &[Output<Self::Custom>]) -> Result<(), Self::Error>;
 }
